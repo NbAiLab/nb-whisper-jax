e70bcfd4f6a5b9d73bfd09c387c29aab930fb470
pere <per@capia.no>
2023-12-13 21:57:48 +0100

    Update demo.py

--- app/demo.py
+++ app/demo.py
@@ -537,39 +537,38 @@ if __name__ == "__main__":
         description=description,
         article=article,
     )
-    
+
+    # Advanced options components
+    num_beams_slider = gr.Slider(minimum=1, maximum=10, step=1, label="Number of Beams", value=3)
+    temperature_slider = gr.Slider(minimum=0, maximum=3, step=0.1, label="Temperature", value=1.0)
+
     # Advanced options inside an Accordion
-    advanced_options = gr.Accordion([
-        gr.Slider(minimum=1, maximum=10, step=1, label="Number of Beams", value=3),
-        gr.Slider(minimum=0, maximum=3, step=0.1, label="Temperature", value=1.0)
-    ], label="Advanced Options", open=False)  # 'open=False' ensures it's collapsed by default
-    
-    # Define inputs including the Accordion
-    inputs = [
-        gr.Audio(sources=["upload", "microphone"], label="Audio file", type="filepath"),
-        gr.Radio(["Bokmål", "Nynorsk", "English"], label="Output Language", value="Bokmål"),
-        gr.Checkbox(value=True, label="Return timestamps"),
-        advanced_options
-    ]
-    
-    # Define outputs
-    outputs = [
-        gr.Video(label="Video", visible=True),
-        gr.Audio(label="Audio", visible=False),
-        gr.Textbox(label="Transcription", show_copy_button=True, show_label=True),
-        gr.Textbox(label="Transcription Time (s)"),
-        gr.File(label="Download")
-    ]
-    
-    # Create the Interface
+    advanced_options = gr.Accordion(
+        gr.Row([num_beams_slider, temperature_slider]),
+        open=False,
+        label="Advanced Options"
+    )
+
+    # Define your Gradio interfaces
     audio_chunked = gr.Interface(
         fn=transcribe_chunked_audio,
-        inputs=inputs,
-        outputs=outputs,
+        inputs=[
+            gr.Audio(sources=["upload", "microphone"], label="Audio file", type="filepath"),
+            gr.Radio(["Bokmål", "Nynorsk", "English"], label="Output Language", value="Bokmål"),
+            gr.Checkbox(value=True, label="Return timestamps"),
+            advanced_options  # Add the Accordion here
+        ],
+        outputs=[
+            gr.Video(label="Video", visible=True),
+            gr.Audio(label="Audio", visible=False),
+            gr.Textbox(label="Transcription", show_copy_button=True, show_label=True),
+            gr.Textbox(label="Transcription Time (s)"),
+            gr.File(label="Download")
+        ],
         allow_flagging="never",
         title=title,
         description=description,
-        article=article
+        article=article,
     )
     
 
